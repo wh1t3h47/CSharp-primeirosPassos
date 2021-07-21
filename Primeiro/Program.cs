b4ff58e2f5a93b8b57d130d23bafe790e59cbe23
juliofilizzola <juliofilizzola@Hotmail.com>
2021-07-20 21:43:26 -0300

    add variaveis

--- Primeiro/Program.cs
+++ Primeiro/Program.cs
@@ -3,6 +3,12 @@
 namespace Primeiro {
     class Program {
         static void Main(string[] args) {
+
+            bool completo = true;
+            byte n1 = 126;
+            int n2 = 1000;
+
+
             Console.WriteLine("Olá Mundo!");
         }
     }
